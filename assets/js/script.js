c22b3d0bb3f9895b3f24b63a8f969d3472d38648
0x000alek <aolguin.or@gmail.com>
2024-12-22 22:07:41 -0300

    delete task commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,32 +5,54 @@ var completedTasks = document.querySelector('.completed-tasks')
 var taskDescriptionInput = document.querySelector('#task-description')
 var toDoListContainer = document.querySelector('#to-do-list')
 
-var taskLastId = 1
-var toDoList = []
+var toDoList = [
+    {
+        id: 1,
+        description: 'Hacer mercado',
+        done: true
+    },
+    {
+        id: 2,
+        description: 'Estudiar para la prueba',
+        done: false
+    },
+    {
+        id: 3,
+        description: 'Sacar a pasear a Tobby',
+        done: false
+    }
+]
 
 totalTasks.innerHTML = 0
 completedTasks.innerHTML = 0
 
 addTaskBtn.addEventListener('click', () => {
-    let taskId = taskLastId++
+    let taskId = toDoList[toDoList.length - 1].id
     let taskDescription = taskDescriptionInput.value
 
+    taskId++
     toDoList.push({id: taskId, description: taskDescription, done: false})
 
     taskDescriptionInput.value = ''
-    totalTasks.innerHTML = toDoList.length
     
-    refreshToDoList()
+    showToDoList()
 })
 
-const refreshToDoList = () => {
+const updateTaskIndicators = () => {
+    totalTasks.innerHTML = toDoList.length
+    completedTasks.innerHTML = toDoList.filter(t => t.done).length
+}
+
+const showToDoList = () => {
     toDoListContainer.innerHTML = ""
     toDoList.forEach(e => {
         let taskElement = document.createElement('div')
         let taskContent = `<div class="col-xl-3"><p> ${e.id} </p></div>`
             + `<div class="col-xl-6"><p class="${e.done ? 'text-success text-decoration-line-through' : ''}"> ${e.description} </p></div>`
-            + `<div class="col-auto"><input class="form-check-input" type="checkbox" ${e.done ? 'checked' : ''} onchange='changeTaskState(${e.id})'></div>`
-            + `<div class="col-auto"><button type="button" class="btn btn-danger" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;"><i class="fa-solid fa-trash"></i></button></div>`
+            + `<div class="col-auto"><input class="form-check-input" type="checkbox" ${e.done ? 'checked' : ''} onchange="changeTaskState(${e.id})"></div>`
+            + `<div class="col-auto"><button type="button" class="btn btn-danger" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;" `
+            + `onclick="deleteTask(${e.id})">`
+            + `<i class="fa-solid fa-trash"></i></button></div>`
 
         taskElement.classList.add('row')
         taskElement.classList.add('justify-content-start')
@@ -39,13 +61,22 @@ const refreshToDoList = () => {
 
         toDoListContainer.appendChild(taskElement)
     })
+
+    updateTaskIndicators()
 }
 
 const changeTaskState = (id) => {
     let wantedTask = toDoList.find(t => t.id == id)
     wantedTask.done = !wantedTask.done
 
-    completedTasks.innerHTML = toDoList.filter(t => t.done).length
-
-    refreshToDoList()
+    showToDoList()
 }
+
+const deleteTask = (id) => {
+    let wantedTaskId = toDoList.findIndex(t => t.id == id)
+    toDoList.splice(wantedTaskId, 1)
+
+    showToDoList()
+}
+
+showToDoList()
